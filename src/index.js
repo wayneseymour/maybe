b2d19c44930058f13b15a4f7a1e2159242a8e914
Tre' Seymour <wayne.seymour@elastic.co>
2020-07-08 11:41:40 -0600

    A lil cleanup, again and add editorconfig

--- src/index.js
+++ src/index.js
@@ -3,53 +3,51 @@
 /* eslint no-unused-vars: 0 */
 
 export const Just = x => ({
-    value: () => x,
-    map: (f) => Maybe.of(f(x)),
-    isJust: () => true,
-    inspect: () => `Just(${x})`,
+  value: () => x,
+  map: (f) => Maybe.of(f(x)),
+  isJust: () => true,
+  inspect: () => `Just(${x})`,
 })
 
 Just.of = function of(x) {
-    return Just(x);
+  return Just(x);
 };
 
 export function just(x) {
-    return Just.of(x);
+  return Just.of(x);
 }
 
 export const Maybe = x => ({
-    chain: (f) => f(x),
-    map: (f) => Maybe(f(x)),
-    inspect: () => `Maybe(${x})`,
-    nothing: () => Nothing(),
-    isNothing: () => false,
-    isJust: () => false,
+  chain: (f) => f(x),
+  map: (f) => Maybe(f(x)),
+  inspect: () => `Maybe(${x})`,
+  nothing: () => Nothing(),
+  isNothing: () => false,
+  isJust: () => false,
 })
 
 Maybe.of = function of(x) {
-    return just(x);
+  return just(x);
 };
 
 export function maybe(x) {
-    return Maybe.of(x);
+  return Maybe.of(x);
 }
 
 export function fromNullable(x) {
-    return x !== null && x !== undefined && x !== false && x !== 'undefined' ? just(x) : nothing();
+  return x !== null && x !== undefined && x !== false && x !== 'undefined' ? just(x) : nothing();
 }
 
-export function Nothing() {
-    return {
-        value: () => {
-            throw new TypeError(`Nothing algebraic data type returns...no value :)`);
-        },
-        map: (f) => {
-        },
-        isNothing: () => true,
-        inspect: () => `[Nothing]`,
-    };
-}
+export const Nothing = () => ({
+  value: () => {
+    throw new TypeError(`Nothing algebraic data type returns...no value :)`);
+  },
+  map: (f) => {
+  },
+  isNothing: () => true,
+  inspect: () => `[Nothing]`,
+})
 
 export function nothing() {
-    return Nothing();
+  return Nothing();
 }
